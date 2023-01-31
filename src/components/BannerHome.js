17f710ed23aec6868907c02c873b6ed21b6899e4
nadianielsen <U2404561@rts-365.dk>
2023-01-31 08:28:01 +0100

    banner - trying figure out some styling

--- src/components/BannerHome.js
+++ src/components/BannerHome.js
@@ -6,15 +6,16 @@ import { Link } from "react-router-dom";
 
 const BannerHome = () => {
 
-    const {data, loading, error} = useAxios(`https://api.themoviedb.org/3/movie/popular?api_key=75f15351c6119a96302b866663e596b0&language=en-US&page=1`)
+    const {data, loading, error} = useAxios(`https://api.themoviedb.org/3/person/{person_id}/images?api_key=75f15351c6119a96302b866663e596b0`)
+    console.log(data)
 
     return ( 
-    <div className="bg-transparent w-[56rem] h-72 rounded-3xl ml-5 mt-10 flex justify-end items-end bg-gradient-to-b from-neutral-700 bg-blend-multiply">
-        {/* {error && <p>{error}</p>}
+    <div className="bg-transparent w-[56rem] h-72 rounded-3xl ml-5 mt-10 flex justify-end items-end bg-gradient-to-b from-neutral-700 bg-blend-multiply relative aspect-video">
+        {error && <p>{error}</p>}
             {loading && <p>Loading...</p>}
             {data?.results?.map(movie => (
-                // <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt="" className="" />
-                ))} */}
+                <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt="" className="absolute w-full h-full rounded-3xl object-cover" />
+                ))}
                 <button className="bg-neutral-300 bg-opacity-20 w-32 h-14 rounded-2xl backdrop-blur-sm mb-4 mr-[2rem] box-shadow">
                     <span className="flex gap-4 ml-7 text-white">
                     <FaPlay className="mt-1"/> 
--- src/components/PopularMovies.js
+++ src/components/PopularMovies.js
@@ -1,5 +1,6 @@
 import useAxios from "../customHooks/useAxios";
 import { Link } from "react-router-dom";
+import {IoIosStar} from "react-icons/io"
 
 
 const PopularMovies = () => {
@@ -18,7 +19,17 @@ const PopularMovies = () => {
         {error && <p>{error}</p>}
         {loading && <p>Loading...</p>}
         {data?.results?.map((movie, index) => {
-               if (index < 4 ) return (<img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt="" className="w-48 h-72 rounded-3xl m-5 relative" />)
+               if (index < 4 ) return (
+                <Link to={"/detailmovies"} className="text-none">
+                <div className="relative">
+                    <div className="bg-neutral-300 bg-opacity-20 w-20 h-7 rounded-tr-3xl rounded-bl-3xl ml-[6.99rem] flex justify-evenly backdrop-blur-sm absolute left-5">
+                        <IoIosStar className="text-yellow-400 text-2xl"/>
+                        <h3 className="text-xs pt-1.5 text-white">{movie.vote_average}</h3>
+                    </div>
+                    <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt="" className="w-48 h-72 rounded-3xl m-5" />
+                </div>
+            </Link>             
+                )
             })}
         </article>
             </article>
