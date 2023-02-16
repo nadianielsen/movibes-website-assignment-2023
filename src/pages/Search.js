f06bfcbd35c9be008a0d367c7d2683a004d34b3e
nadianielsen <U2404561@rts-365.dk>
2023-02-16 12:39:01 +0100

    fixed something on tvseries even tho it's not doing the right thing

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -5,6 +5,7 @@ import SearchForm from "../components/SearchForm";
 import NotificationProfileSection from "../components/NotificationProfileSection";
 import { Link } from "react-router-dom";
 import { IoIosStar } from "react-icons/io"
+import ActorModal from "../components/ActorModal";
 
 const Search = () => {
 
@@ -16,6 +17,15 @@ const Search = () => {
     const [tvSeries, setTvSeries] = useState();
     const [error, setError] = useState();
     const [loading, setLoading] = useState(true);
+    const [actorId, setActorId] = useState();
+    const [isActorModalOpen, setIsActorModalOpen] = useState(false);
+
+    const handleShowMember = (id) => {
+        setActorId(id)
+        setIsActorModalOpen(true)
+    }
+
+    const closeActorModal = () => setIsActorModalOpen(false)
 
     useEffect(() => {
         axios(`https://api.themoviedb.org/3/search/multi?api_key=75f15351c6119a96302b866663e596b0&query=${query}`)
@@ -66,7 +76,7 @@ const Search = () => {
                     <h1 className="text-neutral-200 font-bold">People</h1>
                     <article className="grid grid-cols-5 gap-y-5">
                     {people.map(person => (
-                        <div className="w-36 h-36 rounded-full my-6">
+                        <div className="w-36 h-36 rounded-full my-6" onClick={() => {handleShowMember(person.id)}}>
                             <img src={`https://image.tmdb.org/t/p/original${person.profile_path}`} alt="" className="rounded-full w-full h-full object-cover"/>
                             <h2 className="text-center text-neutral-200">{person.name}</h2>
                         </div>
@@ -79,19 +89,26 @@ const Search = () => {
                 <h1 className="text-neutral-200 font-bold">TV Series</h1>
                 <article className="mt-6 grid grid-cols-5 gap-8">
                 {tvSeries.map(tvshow => (
-                        <div className="hover:scale-110 transition-all">
-                        <div className="relative">
-                            <div className="bg-neutral-300 bg-opacity-20 w-20 h-7 rounded-tr-3xl rounded-bl-3xl ml-[6.99rem] flex justify-evenly backdrop-blur-sm absolute left-5">
-                                <IoIosStar className="text-yellow-400 text-2xl"/>
-                                <h3 className="text-xs pt-1.5 text-white black-text-shadow">{tvshow.vote_average}</h3>
-                            </div>
-                            <img src={`https://image.tmdb.org/t/p/original${tvshow.poster_path}`} alt="" className="w-48 h-72 m-5 rounded-3xl hover:black-box-shadow " />
-                        </div>
-                        </div>
+                         <Link to={`/detailmovies/${tvshow.id}`} className="text-none">
+                         <div className="hover:scale-110 transition-all">
+                         <div className="relative">
+                             <div className="bg-neutral-300 bg-opacity-20 w-20 h-7 rounded-tr-3xl rounded-bl-3xl ml-[6.99rem] flex justify-evenly backdrop-blur-sm absolute left-5">
+                                 <IoIosStar className="text-yellow-400 text-2xl"/>
+                                 <h3 className="text-xs pt-1.5 text-white black-text-shadow">{tvshow.vote_average}</h3>
+                             </div>
+                             <img src={`https://image.tmdb.org/t/p/original${tvshow.poster_path}`} alt="" className="w-48 h-72 m-5 rounded-3xl hover:black-box-shadow " />
+                         </div>
+                         </div>
+                     </Link>
                     ))}
                 </article>
                 </div>
             ): null}
+              <ActorModal
+            isActorModalOpen={isActorModalOpen}
+            closeActorModal={closeActorModal}
+            actorId={actorId}
+            />
         </article>
     )
 }
